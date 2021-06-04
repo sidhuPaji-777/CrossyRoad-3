41a0c68ca2810f1e18472a486e33b293e76cb392
sidhuPaji-777 <82391641+sidhuPaji-777@users.noreply.github.com>
2021-06-04 16:04:03 +0530

    Update Player.js

--- Player.js
+++ Player.js
@@ -6,6 +6,8 @@ class Player{
         this.y = y;
         this.spt = createSprite(this.x, this.y, 20, 50);
         this.spt.shapeColor = "Yellow";
+        this.spt.addAnimation("player", playerAnimation);
+        this.spt.scale = 0.07;
     }
 
     move(xdir, ydir)
